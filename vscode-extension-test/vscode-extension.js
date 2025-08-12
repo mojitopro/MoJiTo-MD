bf42dc15a4a735de8e4e295f0ee194d68aff15f1
mojitopro <mojitop25@gmail.com>
2025-08-12 03:51:00 -0300

    Delete

--- vscode-extension-test/vscode-extension.js
+++
@@ -1,7 +0,0 @@
-export function activate(context) {
-  window.acode.alert("Extensión activa", "La API de VSCode ya funciona dentro de Acode.");
-}
-
-export function deactivate() {
-  window.acode.alert("Extensión desactivada", "La extensión fue desactivada.");
-}
